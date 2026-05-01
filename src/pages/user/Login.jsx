455d03aa94dd9e2ad2e6720e519af0c50a1c1430
anantharjun672001-a11y <anantharjun672001@gmail.com>
2026-05-01 12:47:22 +0530

    api changed

--- src/pages/user/Login.jsx
+++ src/pages/user/Login.jsx
@@ -18,7 +18,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
   e.preventDefault();
 
-  const res = await axios.post("http://localhost:5000/api/auth/login", form);
+  const res = await axios.post("http://localhost:3000/api/auth/login", form);
 
   localStorage.setItem("token", res.data.token);
   localStorage.setItem("user", JSON.stringify(res.data.user));
