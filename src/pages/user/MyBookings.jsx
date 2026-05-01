fd90f036db622e1fdb0458b719c0b2ca1ac947b2
anantharjun672001-a11y <anantharjun672001@gmail.com>
2026-05-01 12:34:31 +0530

    Mybookings created successfully

--- src/pages/user/MyBookings.jsx
+++ src/pages/user/MyBookings.jsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const MyBookings = () => {
-    return (
-        <div>
-            <h1>My Bookings</h1>
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchBookings = async () => {
+      const token = localStorage.getItem("token");
+
+      const res = await axios.get(
+        "http://localhost:3000/api/bookings/my",
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      setData(res.data);
+    };
+
+    fetchBookings();
+  }, []);
+
+  return (
+    <div>
+      <h2>My Bookings</h2>
+
+      {data.map((b) => (
+        <div key={b._id}>
+          <p>{b.service}</p>
+          <p>{b.date}</p>
+          <p>{b.status}</p>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default MyBookings;
